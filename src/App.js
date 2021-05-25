c73bb76f50cb5809af4c8c353c0dde8937b0a266
wizksy <wizksy@gmail.com>
2021-05-25 21:51:29 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,65 +1,7 @@
-import React, { useState } from 'react';
-import Detail from './Detail';
-import './App.css';
+import React from 'react';
+import Home from './Home';
 
 function App() {
-  const title = '용이보드';
-  return (
-    <div className="App">
-      <div className="header">{title}</div>
-      <Body
-        list={[
-          { title: 'React Basic Step1', date: '2021.05.21' },
-          { title: 'React Basic Step1', date: '2021.05.21' },
-          { title: 'React Basic Step1', date: '2021.05.21' },
-        ]}
-      ></Body>
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
-
-{
-  /* 본문 컴포넌트 */
-}
-function Body(testList) {
-  const [boardListData, setBoardListData] = useState([
-    { title: 'React Basic Step1', date: '2021.05.21' },
-    { title: 'React Basic Step1', date: '2021.05.21' },
-    { title: 'React Basic Step1', date: '2021.05.21' },
-  ]);
-
-  console.log(testList);
-  const boardList = boardListData.map((data) => (
-    <Row title={data.title} date={data.date} />
-  ));
-  return <div>{boardList}</div>;
-}
-
-{
-  /* 게시판 Row  */
-}
-function Row(props) {
-  const { title, date } = props;
-  return (
-    <div
-      className="list"
-      onClick={(e) => {
-        console.log('click event');
-        console.log(e);
-      }}
-      onChange={(e) => {
-        console.log('change');
-      }}
-    >
-      <div>{title}</div>
-      <div className="listDate">{date}</div>
-    </div>
-  );
-}
-
-function Footer() {
-  return <div className="footer">copyright wizksy</div>;
-}
-
 export default App;
