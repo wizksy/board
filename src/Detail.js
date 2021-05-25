011a1a334a0957d8fbd70907feeef968e3b8cc61
김성용 <wizksy@gmail.com>
2021-05-25 14:48:16 +0000

    test

--- src/Detail.js
+++ src/Detail.js
@@ -1,20 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Detail = () => {
-	const [message, setMessage] = useState('');
-	const onClickEnter = () => setMessage('Hello');
-	const onClickLeave = () => setMessage('Good Bye~');
-	const [color, setColor] = useState('black');
-  return 
-	<div>
-		<div>detail page</div>
-		<button onClick={onClickEnter}>Enter</button>
-	  	<button onClick={onClickLeave}>Leave</button>
-	  	<input type="text" name="message" placeholder="input anything" onChange={
-			  (e) => {console.log(e)}
-		  }
-		/>
-  	</div>;
+  const [message, setMessage] = useState('');
+  const onClickEnter = () => setMessage('Hello');
+  const onClickLeave = () => setMessage('Good Bye~');
+  const [color, setColor] = useState('black');
+  return (
+    <div>
+      <div>detail page</div>
+      <button onClick={onClickEnter}>Enter</button>
+      <button onClick={onClickLeave}>Leave</button>
+      <input
+        type="text"
+        name="message"
+        placeholder="input anything"
+        onChange={(e) => {
+          console.log(e);
+        }}
+      />
+    </div>
+  );
 };
 
 export default Detail;
